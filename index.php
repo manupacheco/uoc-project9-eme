75b371e7eb102bc4dc9b0000e3f79a886b59f4d5
Manu Pacheco <mpachecopal@gmail.com>
2018-11-17 11:45:46 +0100

    add bootsrap and partials

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Productos y valoraciones</title>
-</head>
-<body>
-  
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php 
+      include 'partials/css-imports.php';
+    ?>
+    <title>MVALORACIONES</title>
+  </head>
+  <body>
+    <h1>MVALORACIONES</h1>
+
+    <?php 
+      include 'partials/footer.php';
+    ?>
+  </body>
 </html>
