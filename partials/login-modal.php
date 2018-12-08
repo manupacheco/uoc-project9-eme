5a77980b664b75647bb3472c71bd40ed4bf888a6
Manu Pacheco <mpachecopal@gmail.com>
2018-12-08 12:44:55 +0100

    add login

--- partials/login-modal.php
+++ partials/login-modal.php
@@ -8,12 +8,18 @@
         </button>
       </div>
       <div class="modal-body">
-        User:
-        Password:
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-info">Login</button>
+        <form>
+          <div class="form-group">
+            <label for="inputEmail">Email</label>
+            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label for="inputPassword">Password</label>
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+          </div>
+          <!-- <button type="button" class="btn btn-light" data-dismiss="modal">Close</button> -->
+          <button type="submit" class="btn btn-info">Login</button>
+        </form>
       </div>
     </div>
   </div>
