471c31d17333800e9f9c07218ddb39c15a84f773
Manu Pacheco <mpachecopal@gmail.com>
2018-12-08 12:34:39 +0100

    add products

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark">
   <div class="container">
-    <a class="navbar-brand" href="#">M-Valoraciones</a>
+    <a class="navbar-brand" href="#">MValoraciones</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -30,7 +30,7 @@
       </ul>
       <form class="form-inline my-2 my-lg-0">
         <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
-        <button class="btn btn-outline-success my-2 my-sm-0">Login</button>
+        <button class="btn btn-light my-2 my-sm-0" data-toggle="modal" data-target="#loginModal">Login</button>
       </form>
     </div>
   </div>
