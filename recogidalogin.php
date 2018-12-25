b094ce4b86b0be5e7f5dfbe79aa942a60623706a
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-25 17:18:39 +0100

    Add files via upload

---
+++ recogidalogin.php
@@ -0,0 +1,56 @@
+<?php
+
+   
+
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+   
+$db = 'eme'; 
+$host = 'localhost'; 
+$user = 'root'; 
+$pass = ''; 
+$conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// iniciar transacción 
+$conn->beginTransaction();
+ switch (empty($usuario)||empty($password)) {
+     
+ 
+case true:
+     echo "Campos obligatorios sin rellenar <br/>";
+ 
+case false:
+try { 
+// tabla 1 
+$sql = "SELECT IdUsuario FROM usuarios WHERE usuario = :usuario AND password = :password ;"; 
+$result = $conn->prepare($sql); 
+$result->bindValue(':usuario', $usuario, PDO::PARAM_STR);
+$result->bindValue(':password', $password, PDO::PARAM_STR);
+$result->execute(); 
+$count = $result->rowCount();
+$data=$result->fetch(PDO::FETCH_OBJ);
+
+
+
+
+$conn->commit(); 
+
+if($count)
+{
+$_SESSION['IdUsuario']=$data->IdUsuario; // Storing user session value
+
+header("Location: index.php"); 
+
+
+}
+else
+{
+echo "Usuario no registrado";  
+} 
+}
+ 
+catch(PDOException $e) {
+echo '{"error":{"text":'. $e->getMessage() .'}}';
+}
+ }
