ceb334fc0790487a74679b82c1e469992d74c723
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-25 20:09:11 +0100

    Add files via upload

---
+++ selec_producto.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$db = 'eme'; 
+$host = 'localhost'; 
+$user = 'root'; 
+$pass = ''; 
+$conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// iniciar transacción 
+$conn->beginTransaction();
+
+try { 
+
+$sql = "SELECT enlace, precio, titulo, descripcion FROM imagenes INNER JOIN productos WHERE imagenes.idProducto3 = productos.IdProducto;"; 
+$result = $conn->prepare($sql); 
+$result->execute(); 
+
+
+
+
+
+
+
+
+$conn->commit(); 
+
+foreach ($result as $row){
+    
+ 
+}
+}
+
+catch(PDOException $e) {
+echo '{"error":{"text":'. $e->getMessage() .'}}';
+}
+
