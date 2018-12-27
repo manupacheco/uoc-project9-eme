7a275758a2928b1bcfa08571fdbbf36b12e81f93
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-27 14:59:50 +0100

    Add files via upload

---
+++ datosusuario.php
@@ -0,0 +1,85 @@
+<!Doctype html>
+<html>
+<head>
+    
+    <title>formulario</title> 
+    <meta charset="UTF-8">
+    <?php include 'session.php'; ?>
+    
+
+
+</head>
+
+<body>
+    
+    
+</body>
+
+<?php
+ 
+$db = 'eme'; 
+$host = 'localhost'; 
+$user = 'root'; 
+$pass = ''; 
+$conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// iniciar transacción 
+$conn->beginTransaction();
+
+try { 
+
+$sql = "SELECT * FROM usuarios WHERE usuario like :usuario ;"; 
+$result = $conn->prepare($sql); 
+$result->bindValue(':usuario', $usuario, PDO::PARAM_STR);
+$result->execute(); 
+$fila = $result->fetch(PDO::FETCH_BOTH);
+
+
+
+
+
+
+
+$conn->commit(); 
+
+
+}
+
+catch(PDOException $e) {
+echo '{"error":{"text":'. $e->getMessage() .'}}';
+}
+?>
+<div class="container">
+<h2>Modificar datos personales</h2>
+<form action="modifdatosusuario.php" method="post">
+    
+    
+    <label for = "usuario"> Usuario</label>
+    <input type = "text"   name = "usuario" id = "usuario" size = "30" value="<?php echo $fila[2];?>" ><br><br>
+    
+     <label for = "password"> Contraseña</label>
+    <input type = "password"   name = "password" id = "password" size = "30" value="<?php echo $fila[3];?>" ><br><br>
+    
+    <label for = "nombre"> Nombre</label>
+    <input type = "text"   name = "nombre" id = "nombre" size = "30" value="<?php echo $fila[4];?>" ><br><br>
+   
+    <label for = "apellidos"> Apellidos</label>
+    <input type = "text"   name = "apellidos" id = "apellidos" size = "30" value="<?php echo $fila[5];?>" ><br><br>
+    
+    <label for = "mail"> Usuario</label>
+    <input type = "email"   name = "mail" id = "mail" size = "30" value="<?php echo $fila[6];?>" ><br><br>
+    
+    <label for = "usuario"> Usuario</label>
+    <input type = "text"   name = "usuario" id = "usuario" size = "30" value="<?php echo $fila[7];?>" ><br><br>
+    
+    <label for = "telefono"> Telefono</label>
+    <input type = "text"   name = "telefono" id = "telefono" size = "30" value="<?php echo $fila[8];?>" ><br><br>
+    
+    <button type ="button" name="modificar">Modificar datos</button>
+</form>
+
+</div>
+
+
+</html>
