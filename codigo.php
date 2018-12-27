9b51651ea587b87773121f3fbdd83637102a127e
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2018-12-27 21:23:23 +0100

    Add files via upload

---
+++ codigo.php
@@ -0,0 +1,114 @@
+<?php
+
+ 
+
+
+class codigo {   
+    
+public function getusuario () 
+    
+    {
+      
+        return $this->usuario;   
+               
+        
+    }
+    
+    public function setusuario ($usuario)
+    {
+       
+        $this->usuario = $usuario;
+    
+    }
+    
+    public function getpassword () 
+    
+    {
+      
+        return $this->password;   
+               
+        
+    }
+    
+    public function setpassword ($password)
+    {
+       
+        $this->password = $password;
+    
+    }
+ 
+public function login($usuario, $password){
+
+    require_once  "./conexion.php";
+// iniciar transacción 
+$conn->beginTransaction();
+ switch (empty($usuario)||empty($password)) {
+     
+ 
+case true:
+     echo "Campos obligatorios sin rellenar <br/>";
+ 
+case false:
+try { 
+// tabla 1 
+$sql = "SELECT IdUsuario FROM usuarios WHERE usuario = :usuario AND password = :password ;"; 
+$result = $conn->prepare($sql); 
+$result->bindValue(':usuario', $usuario, PDO::PARAM_STR);
+$result->bindValue(':password', $password, PDO::PARAM_STR);
+$result->execute(); 
+$count = $result->rowCount();
+$data=$result->fetch(PDO::FETCH_OBJ);
+
+
+
+
+$conn->commit(); 
+
+if($count)
+{
+$_SESSION['usuario']=$usuario; // Storing user session value
+
+header("Location: datosusuario.php"); 
+
+//echo "Bienvenido $usuario <a href = datosusuario.php> Para modificar sus datos pinche aqui</a>";
+}
+else
+{
+echo "Usuario no registrado";  
+} 
+}
+ 
+catch(PDOException $e) {
+echo '{"error":{"text":'. $e->getMessage() .'}}';
+}
+}
+}
+
+public function seleccionar($usuario){
+    
+include "./conexion.php";   
+$conn->beginTransaction();   
+
+try { 
+// tabla 1 
+$sql = "SELECT * FROM usuarios WHERE usuario = :usuario ;"; 
+$result = $conn->prepare($sql); 
+$result->bindValue(':usuario', $usuario, PDO::PARAM_STR);
+$resultado = $result->execute(); 
+if ($resultado){
+    
+$fila = $result->fetch(PDO::FETCH_BOTH);
+return $fila;
+}
+
+
+
+
+$conn->commit(); 
+   }
+ 
+catch(PDOException $e) {
+echo '{"error":{"text":'. $e->getMessage() .'}}';
+}
+}
+}
