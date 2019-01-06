030da726050523c0ac60da3de34765b17a600fc9
mamen varela <44292320+MamenVarela@users.noreply.github.com>
2019-01-06 13:29:24 +0100

    Update datosusuario.php

--- datosusuario.php
+++ datosusuario.php
@@ -7,6 +7,7 @@
     <?php include 'session.php'; ?>
     <?php require_once ("./conexion.php");?>
     <?php include 'codigo.php';?>
+   
 
 
 </head>
@@ -18,11 +19,15 @@
 
 <?php
  $codigo = new codigo();
- $codigo ->setusuario(filter_input(INPUT_POST, 'usuario'));
- $codigo->setpassword(filter_input(INPUT_POST, 'password'));
- $usuario = $codigo->usuario ;
- $password = $codigo->password ;
+ //$codigo ->setusuario(filter_input(INPUT_POST, 'usuario'));
+ //$usuario = $codigo->usuario ;
+ //$codigo ->setusuario(filter_input(INPUT_POST, $_COOKIE['usuario']));
  $consulta = $codigo->seleccionar($usuario);
+ 
+ 
+ 
+ $usuarioaqui = $_COOKIE['usuario'];
+ 
  
 
 // iniciar transacción 
@@ -34,25 +39,25 @@
     
     
     <label for = "usuario"> Usuario</label>
-    <input type = "text"   name = "usuario" id = "usuario" size = "30" value="<?php echo $consulta['usuario'];?>" ><br><br>
+    <input type = "text"   name = "usuario" id = "usuario" size = "30" value="<?php echo $usuarioaqui;/*$consulta['usuario'];*/?>" ><br><br>
     
      <label for = "password"> Contraseña</label>
     <input type = "password"   name = "password" id = "password" size = "30" value="<?php echo $consulta[3];?>" ><br><br>
     
     <label for = "nombre"> Nombre</label>
-    <input type = "text"   name = "nombre" id = "nombre" size = "30" value="<?php echo $fila[4];?>" ><br><br>
+    <input type = "text"   name = "nombre" id = "nombre" size = "30" value="<?php echo $consulta[4];?>" ><br><br>
    
     <label for = "apellidos"> Apellidos</label>
-    <input type = "text"   name = "apellidos" id = "apellidos" size = "30" value="<?php echo $fila[5];?>" ><br><br>
+    <input type = "text"   name = "apellidos" id = "apellidos" size = "30" value="<?php echo $consulta[5];?>" ><br><br>
     
-    <label for = "mail"> Usuario</label>
-    <input type = "email"   name = "mail" id = "mail" size = "30" value="<?php echo $fila[6];?>" ><br><br>
+    <label for = "mail"> Email</label>
+    <input type = "email"   name = "mail" id = "mail" size = "30" value="<?php echo $consulta[6];?>" ><br><br>
     
-    <label for = "usuario"> Usuario</label>
-    <input type = "text"   name = "usuario" id = "usuario" size = "30" value="<?php echo $fila[7];?>" ><br><br>
+    <label for = "usuario"> Telefono</label>
+    <input type = "text"   name = "telefono" id = "telefono" size = "30" value="<?php echo $consulta[7];?>" ><br><br>
     
-    <label for = "telefono"> Telefono</label>
-    <input type = "text"   name = "telefono" id = "telefono" size = "30" value="<?php echo $fila[8];?>" ><br><br>
+    <label for = "foto"> Foto</label>
+    <input type = "text"   name = "foto" id = "foto" size = "30" value="<?php echo $consulta[8];?>" ><br><br>
     
     <button type ="button" name="modificar">Modificar datos</button>
 </form>
